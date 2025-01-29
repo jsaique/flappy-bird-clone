436364fa209454960e5fb6e3e272da531e24a7ba
jsaique <jsaique11@gmail.com>
2025-01-29 13:13:16 -0500

    Start game key and restart

---
+++ objects/gameover_message.py
@@ -0,0 +1,16 @@
+import pygame.sprite
+
+import assets
+import configs
+from layer import Layer
+
+
+class GameOverMessage(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        self._layer = Layer.UI
+        self.image = assets.get_sprites("gameover")
+        self.rect = self.image.get_rect(
+            center=(configs.SCREEN_WIDTH / 2, configs.SCREEN_HEIGHT / 2)
+        )
+        self.mask = pygame.mask.from_surface(self.image)
+        super().__init__(*groups)
